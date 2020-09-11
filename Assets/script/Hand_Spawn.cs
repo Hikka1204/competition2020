aea0a1240a850d8ae7068fe522934138675f6aa4
Hikka12044021 <11806061@stu.kbc.ac.jp>
2020-09-11 11:56:16 +0900

    プロジェクトの作成

---
+++ Assets/script/Hand_Spawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand_Spawn : MonoBehaviour {
+
+    [SerializeField] private GameObject hand;
+
+    private float RandomDestroyReat;
+    private float DestroyTime;
+   
+
+	// Use this for initialization
+	void Start () {
+        //hand = gameObject.transform.GetChild(0).gameObject;
+        hand.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            hand.gameObject.SetActive(true);
+        }
+
+        RandomDestroyReat = 0;
+    }
+
+}
