e5b58a8bbdc7e3a4bb6e1c099d9fbdebf347507f
Hikka12044021 <11806061@stu.kbc.ac.jp>
2020-09-16 16:50:33 +0900

    新しいシーンを光ファルダに追加、中身はナビ用のステージを作っている

---
+++ Assets/Hikaru/Scripts/nav_Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nav_Enemy : MonoBehaviour
+{
+    UnityEngine.AI.NavMeshAgent Player_Nav;
+    [SerializeField] GameObject Destination;
+    private Animator anim;
+
+
+
+    void Start()
+    {
+        //プレイヤーのNavMeshAgentを取得
+        Player_Nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        //目的地のオブジェクトを取得
+        //Destination = GameObject.Find("Goal");
+        anim = GetComponent<Animator>();
+        anim.SetInteger("battle", 1);//walk/run/moving
+        anim.SetInteger("moving", 1);//walk/run/moving
+        
+    }
+
+    void Update()
+    {
+        //目的地を設定
+        Player_Nav.SetDestination(Destination.transform.position);
+    }
+}
