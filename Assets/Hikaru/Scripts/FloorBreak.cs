c1fe387d9dc0735361d1003757fecc4a7c30289e
Hikka12044021 <11806061@stu.kbc.ac.jp>
2020-09-22 17:57:21 +0900

    新たにシーンを追加。プロト02のシーンを作成。エネミーが部屋に入り消えて、床が壊れるような演出を作った

---
+++ Assets/Hikaru/Scripts/FloorBreak.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class FloorBreak : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void destroyObject()
+    {
+        
+        var random = new System.Random();
+        var min = -3;
+        var max = 3;
+        gameObject.GetComponentsInChildren<Rigidbody>().ToList().ForEach(r => {
+            r.gameObject.SetActive(true);
+            r.isKinematic = false;
+            r.transform.SetParent(null);
+            r.gameObject.AddComponent<AutoDestroy>().time = 2f;
+            var vect = new Vector3(random.Next(min, max), random.Next(0, max), random.Next(min, max));
+            r.AddForce(vect, ForceMode.Impulse);
+            r.AddTorque(vect, ForceMode.Impulse);
+        });
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            for(int i = 0; i < gameObject.transform.childCount; i++)
+            {
+                gameObject.transform.GetChild(i).gameObject.SetActive(true);
+            }
+            destroyObject();
+        }
+    }
+
+}
