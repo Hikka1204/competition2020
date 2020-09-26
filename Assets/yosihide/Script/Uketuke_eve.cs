48ec466ae7a34d2e35e8edef82b5c26ef5f76a15
yoshihidefunakoshi <11807114@stu.kbc.ac.jp>
2020-09-26 16:40:08 +0900

    扉に触れたときにナースコールが鳴るやつと、鍵を取った時に地下へ誘導するコメントの追加

---
+++ Assets/yosihide/Script/Uketuke_eve.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Uketuke_eve : MonoBehaviour
+{
+    private GameObject Key1;
+    public Text Uketuke;
+    
+    void Start()
+    {
+        Uketuke = GameObject.Find("Text").GetComponent<Text>();
+        Uketuke.enabled = false;
+        Key1 = GameObject.Find("Key1");
+        
+    }
+
+    void Update()
+    {
+        
+        //オブジェクトの個数(count)が0になった時
+        if(Input.GetKeyDown("e")&& Key1 ==null)
+        //if(Destroy(obj))
+        {
+            Uketuke.enabled = true;
+            
+        }
+        
+    }
+    
+    
+}
---
+++ Assets/yosihide/Script/nursecall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nursecall : MonoBehaviour
+{
+    //効果音の設定
+    AudioSource audioSource;
+    public AudioClip NurseCallSE;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    
+        void OnTriggerEnter(Collider other)
+        {
+
+            if (other.gameObject.tag == "Player")
+            {
+                Debug.Log("aa");
+                audioSource.PlayOneShot(NurseCallSE);
+            }
+        }
+    
+}
