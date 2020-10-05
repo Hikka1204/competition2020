e1104dea73c4646fc6783f2ab96d1a8a7b5968ac
810shoma <ow8100@gmail.com>
2020-10-05 14:34:05 +0900

    カメラ制御完成

--- Assets/Syouma/Script/Comment/Come_List.cs
+++ Assets/Syouma/Script/Comment/Come_List.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class Come_List : MonoBehaviour
 {
+    [SerializeField] List<string> Name = new List<string>(); // コメントしてる人の名前リスト
+
     [SerializeField] List<string> Sayhello = new List<string>(); // あいさつ系のコメント一覧
     [SerializeField] List<string> Reaction = new List<string>(); // リアクション系のコメント一覧
     [SerializeField] List<string> Hint = new List<string>();     // ヒント系のコメント一覧 
@@ -15,10 +17,13 @@ public class Come_List : MonoBehaviour
     void Start()
     {
         // 各リストの要素数を記憶
+        int Count0 = Name.Count;
         int Count1 = Sayhello.Count;
         int Count2 = Reaction.Count;
         int Count3 = Hint.Count;
 
+        // コメントした人の名前をランダムで入力
+        this.GetComponent<Text>().text += Name[Random.Range(0, Count0)];
         // あいさつ系を入力
         this.GetComponent<Text>().text += Sayhello[Random.Range(0, Count1)]; 
 
