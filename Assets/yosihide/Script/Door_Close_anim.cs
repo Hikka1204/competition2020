44bd0467b39cf7550b98fc10c601e44e1c8d6861
yoshihidefunakoshi <11807114@stu.kbc.ac.jp>
2020-10-16 14:09:53 +0900

    ドアが勝手に閉まる処理のプレハブとスクリプト追加です

---
+++ Assets/yosihide/Script/Door_Close_anim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Close_anim : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private float _speed = 1;  //ドアのアニメーション速度
+
+    void Start()
+    {
+        animator = transform.GetComponent<Animator>();
+        animator.SetBool("Open", true);
+        animator.speed = _speed;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("Open", false);
+            Destroy(gameObject.GetComponent<Door_Close_anim>());
+        }
+    }
+}
