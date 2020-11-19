257cd011a181f50c7dea04a8b6e87f6c2e8aeafe
I_am_4Bit <11807037@stu.kbc.ac.jp>
2020-11-19 14:26:45 +0900

    ドア変更

---
+++ Assets/Rinya/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿public class UnityChanController : MonoBehaviour
+{
+
+    // スプライトレンダラーコンポーネントを入れる
+    SpriteRenderer sr;
+
+    void Start()
+    {
+
+        // スプライトレンダラーのコンポーネントを取得する
+        this.sr = GetComponent<SpriteRenderer>();
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (sr.flipX == true)
+            {
+                sr.flipX = false;
+            }
+            else if (sr.flipX == false)
+            {
+                sr.flipX = true;
+            }
+
+        }
+    }
+}
