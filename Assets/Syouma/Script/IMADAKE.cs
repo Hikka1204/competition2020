04010feedfce6b3ef4e04a5f448143965c0f297d
810shoma <ow8100@gmail.com>
2020-12-07 01:10:27 +0900

    ・動画撮影用にスクリプトを作成した

---
+++ Assets/Syouma/Script/IMADAKE.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMADAKE : MonoBehaviour
+{
+    bool flg = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!flg)
+            {
+                this.GetComponent<GlitchEffect>().enabled = true;
+                flg = true;
+
+            }
+            else
+            {
+                this.GetComponent<GlitchEffect>().enabled = false;
+                flg = false;
+
+            }
+        }
+    }
+}
