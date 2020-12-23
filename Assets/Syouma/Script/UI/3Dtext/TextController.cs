a06295b4e88d717a4e78534b2863620d600e7d17
810shoma <ow8100@gmail.com>
2020-12-23 17:13:30 +0900

    ・ドアには「E」をつけた

---
+++ Assets/Syouma/Script/UI/3Dtext/TextController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+    void Start()
+    {
+        transform.localScale = new Vector3(-0.1f, 0.1f, 0.1f);
+    }
+
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.gameObject.tag == "Hand")
+    //        this.gameObject.SetActive(true);
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Hand")
+    //        this.gameObject.SetActive(false);
+    //}
+}
